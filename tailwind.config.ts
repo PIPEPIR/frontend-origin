635475f9f3989c48d00c7e96f6d9797103161f4d
Teerachet Piriew <pipepiriew4@gmail.com>
2024-10-28 19:45:47 +0700

    Add variable color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,29 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          10: "#4c1401",
+          20: "#7e2102",
+          30: "#b02e02",
+          40: "#e33c03",
+          50: "#fc541b",
+          60: "#fd7b4f",
+          70: "#fda181",
+          80: "#fec6b3",
+          90: "#ffece6",
+        },
+        grey: {
+          10: "#262626",
+          20: "#404040",
+          30: "#595959",
+          40: "#737373",
+          50: "#8c8c8c",
+          60: "#a6a6a6",
+          70: "#bfbfbf",
+          80: "#d9d9d9",
+          90: "#f2f2f2",
+          100: "#ffffff",
+        },
       },
     },
   },
